0bfdd3537b8e7351429493f59c19c74472272867
Arkadiusz Plichta <arkadiusz.plichta@goyello.com>
2013-01-19 12:52:32 +0100

    initial

---
+++ EventOrganizer/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Optimization;
+
+namespace EventOrganizer.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/Libraries/Angular/angular.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/Libraries/Bootstrap/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/Libraries/JQuery/jquery-1.9.0.js"));
+
+            bundles.Add(new StyleBundle("~/styles/bootstrap").Include(
+                "~/Content/bootstrap.css", 
+                "~/Content/bootstrap-responsive.css"));
+        }
+    }
+}
---
+++ EventOrganizer/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using EventOrganizer.App_Start;
+
+namespace EventOrganizer
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
