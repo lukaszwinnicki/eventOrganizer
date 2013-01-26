c5e9fbb9ffc0499e0762fad956d39ece0ac66122
Arkadiusz Plichta <arkadiusz.plichta@goyello.com>
2013-01-26 12:18:54 +0100

    angular ui, group controller

---
+++ EventOrganizer/Controllers/GroupController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EventOrganizer.Controllers
+{
+    public class GroupController : ApiController
+    {
+         public HttpResponseMessage Get()
+         {
+             // GetLoggedUserUser
+             return Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Please log in.");
+         }
+    }
+}
