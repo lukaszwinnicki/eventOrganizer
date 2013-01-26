0b6b4bddb3c203c6e1425dcd85c981d8298562d5
Arkadiusz Plichta <arkadiusz.plichta@goyello.com>
2013-01-26 10:59:00 +0100

    groups mocks

---
+++ EventOrganizer/Models/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace EventOrganizer.Models
+{
+    public class Group
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ EventOrganizer/ViewModels/GroupsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using EventOrganizer.Models;
+using Group = EventOrganizer.Models.Group;
+
+namespace EventOrganizer.ViewModels
+{
+    public class GroupsViewModel
+    {
+        public User User { get; set; }
+        public List<Group> Groups { get; set; }
+    }
+}
