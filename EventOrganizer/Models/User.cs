a82d3e4355ada674f2938cc93307bf42becd67e3
Arkadiusz Plichta <arkadiusz.plichta@goyello.com>
2013-01-26 11:56:41 +0100

    dummy data, my groups page styling

--- EventOrganizer/Models/User.cs
+++ EventOrganizer/Models/User.cs
@@ -4,5 +4,8 @@
     {
         public string Email { get; set; }
         public string Password { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
     }
 }
