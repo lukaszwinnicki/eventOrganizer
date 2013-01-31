d1e7250053b2f99f1f29651abe5067d082c88aff
Łukasz <lukaszwinnicki@gmail.com>
2013-01-31 20:04:24 +0100

    Cornerstone for angular tests
    To run tests "Testacular" is needed.
    
    Run tests command: testacular start ./testacular.conf.js

---
+++ EventOrganizer/Scripts/Tests/Unit/Controllers/GroupsCtrlTest.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+describe('GroupsCtrl', function () {
+    var groupsCtrl,
+        scope,
+        groupResource,
+        loggedUserResource;
+    
+    groupResource = {
+        query: function () { }
+    };
+
+    loggedUserResource = {
+        get: function () { }
+    };
+
+    beforeEach(function () {
+        scope = {};
+        groupsCtrl = new GroupsCtrl(scope, groupResource, loggedUserResource);
+    });
+
+    it('should have empty groups array', function () {
+        expect(scope.groups).toEqual([]);
+    });
+});
