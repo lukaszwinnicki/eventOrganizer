c6d3adeb3d606cbec9cce663a67ce2183f9fb759
Łukasz <lukaszwinnicki@gmail.com>
2013-02-04 22:04:12 +0100

    Going to edit mode on dblClick in group panel

--- EventOrganizer/Scripts/App/Controllers/GroupCtrl.js
+++ EventOrganizer/Scripts/App/Controllers/GroupCtrl.js
@@ -1,11 +1,16 @@
 ﻿function GroupCtrl($scope, $routeParams, groupResource) {
     $scope.group = {};
+    $scope.editMode = false;
 
     if ($routeParams.id) {
         groupResource.get({ id: $routeParams.id }, function (data) {
             $scope.group = data;
         });
     }
+
+    $scope.toggleEditMode = function (enableEditMode) {
+        $scope.editMode = enableEditMode;
+    };
 }
 
 GroupCtrl.$inject = ['$scope', '$routeParams', 'GroupResource'];
