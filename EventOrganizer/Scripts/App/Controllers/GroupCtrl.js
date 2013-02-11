90ee7999c9a4b111609a2f51da9dd37151a6bcaf
Arkadiusz Plichta <el.pikel@gmail.com>
2013-02-11 23:14:07 +0100

    validation, autoupdating

--- EventOrganizer/Scripts/App/Controllers/GroupCtrl.js
+++ EventOrganizer/Scripts/App/Controllers/GroupCtrl.js
@@ -3,11 +3,14 @@
     $scope.editMode = false;
     $scope.events = [];
     $scope.members = [];
+    $scope.newEvent = { };
 
     if ($routeParams.id) {
         groupResource.get({ id: $routeParams.id }, function (data) {
             $scope.group = data;
         });
+
+        createNewEvent();
 
         for (var j = 0; j < 30; j++) {
             var participants = [];
@@ -36,11 +39,36 @@
         for (var i = 0; i < 30; i++) {
             $scope.members.push({ Name: 'Name', Surname: 'Surname', PhotoUrl: '/Content/Images/holder.png' });
         }
+
+        $scope.updateEndDate = function() {
+            $scope.newEvent.EndDate = $scope.newEvent.StartDate;
+        };
+
+        $scope.updateEndHour = function() {
+            $scope.newEvent.SelectedHourEnd = $scope.newEvent.SelectedHourStart;
+        };
     }
 
     $scope.toggleEditMode = function (enableEditMode) {
         $scope.editMode = enableEditMode;
+
+        createNewEvent();
     };
+
+    function createNewEvent() {
+        $scope.newEvent = {
+            SelectedHourStart: '',
+            SelectedHourEnd: '',
+            Hours: [],
+            MaxParticipants: ['Unlimited'],
+            Archivization: ['Automatic', 'Manual']
+        };
+
+        $scope.newEvent.Hours.push('');
+        for (var l = 0; l < 24; l++) {
+            $scope.newEvent.Hours.push(l);
+        }
+    }
 }
 
 GroupCtrl.$inject = ['$scope', '$routeParams', 'GroupResource'];
