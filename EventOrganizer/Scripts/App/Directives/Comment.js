bd2edb94140a1125353e67017e9b0634e4f8a53d
Łukasz <lukaszwinnicki@gmail.com>
2013-04-07 00:58:53 +0200

    Update comments template

--- EventOrganizer/Scripts/App/Directives/Comment.js
+++ EventOrganizer/Scripts/App/Directives/Comment.js
@@ -1,11 +1,15 @@
 var eventOrganizer = eventOrganizer || angular.module('eventOrganizer.Directives', []);
 
-eventOrganizer.directive('eoComment', function () {
+eventOrganizer.directive('eoComment', ['eo.config', function (eoConfig) {
     return {
         rectrict: 'A',
         scope: {
             comment: '='
         },
-        templateUrl: '/Content/Partials/Directives/Comment.html'
+        templateUrl: '/Content/Partials/Directives/Comment.html',
+        controller: function($scope) {
+            var user = $scope.comment.User;
+            user.PhotoUrl = user.PhotoUrl || eoConfig.images.userSmallPlaceholder;
+        }
     };
-});
+}]);
